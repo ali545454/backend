ec712a6eea8c2fa8d42b6764a9ab2d5d7149da77
Ali Elhfni <aaboudooh@gmail.com>
2026-03-15 06:34:43 +0000

    fix routes problems

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,3 +1,6 @@
+from flask import send_from_directory
+import os
+
 from .auth_routes import auth_bp
 from .apartment_routes import apartment_bp
 from .reviews_routes import review_bp
@@ -6,30 +9,38 @@ from .favorite_routes import favorites_bp
 from .image_routes import image_bp
 from .neighborhood_routes import neighborhood_bp
 from .admin_routes import admin_bp
-
-# لو Blueprint موجود في routes/views.py
 from .views_routes import views_bp
-
-# ىخفاهىل
-from flask import send_from_directory
-import os
 
 
 def register_static_routes(app):
+
     @app.route("/uploads/<path:filename>")
     def serve_uploaded_file(filename):
-        return send_from_directory(os.path.join(os.getcwd(), "uploads"), filename)
+        return send_from_directory(app.config["UPLOAD_FOLDER"], filename)
 
 
 def register_routes(app):
-    app.register_blueprint(admin_bp, url_prefix="/api/admin")
-    app.register_blueprint(favorites_bp, url_prefix="/api/v1/")
+
+    # Auth
     app.register_blueprint(auth_bp, url_prefix="/api/v1/auth")
-    app.register_blueprint(apartment_bp, url_prefix="/api/v1/apartments")
-    app.register_blueprint(views_bp)
-    app.register_blueprint(review_bp, url_prefix="/api/v1/reviews")
+
+    # Users
     app.register_blueprint(user_bp, url_prefix="/api/v1/user")
-    # في app/routes/__init__.py
-    app.register_blueprint(neighborhood_bp, url_prefix="/api/v1/")
-    # ✅ لتفعيل الصور
+
+    # Apartments
+    app.register_blueprint(apartment_bp, url_prefix="/api/v1/apartments")
+    app.register_blueprint(image_bp, url_prefix="/api/v1/images")
+    app.register_blueprint(review_bp, url_prefix="/api/v1/reviews")
+    app.register_blueprint(favorites_bp, url_prefix="/api/v1/favorites")
+
+    # Neighborhoods
+    app.register_blueprint(neighborhood_bp, url_prefix="/api/v1/neighborhoods")
+
+    # Admin
+    app.register_blueprint(admin_bp, url_prefix="/api/v1/admin")
+
+    # Views
+    app.register_blueprint(views_bp)
+
+    # Static uploads
     register_static_routes(app)
